fcc7af925181426a988293f07f6576c92d55471e
Fabiano Ramos <fabiano.suporteinfo@gmail.com>
2020-04-26 11:30:57 -0300

    Add methods GET,POST,PUT,DELETE and Filters

--- src/index.js
+++ src/index.js
@@ -7,7 +7,13 @@ app.use(express.json())
 const projects = [];  //Armazenamento na memória
 
 app.get('/projects', (request, response) => {
-  return response.json(projects)
+  const { title } = request.query
+
+  const results = title
+    ? projects.filter(project => project.title.includes(title))
+    : projects
+
+  return response.json(results)
 });
 
 app.post('/projects',(request, response) => {
@@ -20,22 +26,35 @@ app.post('/projects',(request, response) => {
 });
 
 app.put('/projects/:id',(request, response) => {
-  const {id} = request.params
+  const { id } = request.params
+  const { title, owner } = request.body
 
-  console.log(id)
+  const projectIndex = projects.findIndex(project => project.id === id)
 
-    return response.json([
-    'Projeto 4',
-    'Projeto 2',
-    'Projeto 3',
-  ])
+  if(projectIndex < 0) {
+    return response.status(400).json({ error: 'project not found'})
+  }
+
+  const project = { id, title, owner }
+
+  projects[projectIndex] = project
+
+  return response.json(project)
+
 });
 
 app.delete('/projects/:id',(request, response) => {
-  return response.json([
-    'Projeto 2',
-    'Projeto 3',
-  ])
+  const { id } = request.params
+
+  const projectIndex = projects.findIndex(project => project.id === id)
+
+  if(projectIndex < 0) {
+    return response.status(400).json({ error: 'project not found'})
+  }
+
+  projects.splice(projectIndex, 1)
+
+  return response.status(204).send()
 });
 
 
